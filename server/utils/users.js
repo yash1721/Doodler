ee67908a0d2cf692f10d7bad225f63e4dc7b02c9
yash1721 <132601575+yash1721@users.noreply.github.com>
2024-09-05 11:31:58 +0530

    feat: Implement basic real-time chat feature

---
+++ server/utils/users.js
@@ -0,0 +1,53 @@
+const users = [];
+
+// Join user to chat
+
+const addUser = ({name, userId,image, roomId, host, presenter, socketId, score}) => {
+  let boolean = false;
+  users.forEach(element => {
+    if(element.name===name) {
+      boolean = true;
+    }
+  });
+  if(boolean) return users.filter((user)=>user.roomId===roomId);
+  const user = {name, userId, image, roomId, host, presenter, socketId, score};
+  users.push(user);
+  users.sort((a, b) => a.name.localeCompare(b.name));
+  return users.filter((user)=>user.roomId===roomId);
+};
+
+// User leaves chat
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.socketId === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const removeUserbyname = (name, roomId) => {
+  const index = users.findIndex((user) => user.name === name && user.roomId === roomId);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+//get a user from the list
+const getUser = (id)=>{
+    return users.find((user)=>user.socketId===id);
+}
+
+//get all users from the room
+const getUsersInRoom = (roomId)=>{
+    return users.filter((user)=>user.roomId===roomId);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+  removeUserbyname
+};
